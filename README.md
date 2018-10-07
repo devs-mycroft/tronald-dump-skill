2baa2a12ffb1d9e8167ff6a0060947c2d1311e15
Kris Gesling <krisgesling@gmail.com>
2018-10-07 20:07:19 +0930

    v0.1

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
-## Tronald Dump
-Real quotes from the president of the usa
+# <img src='https://rawgithub.com/FortAwesome/Font-Awesome/master/advanced-options/raw-svg/solid/meh-rolling-eyes.svg' card_color='#EDA624' width='50' height='50' style='vertical-align:bottom'/> Tronald Dump
+Talk to the President of the USA.
 
-## Description
-Real quotes from donald trump, president of the usa, courtesy of the tronalddump.io api.
+## About
+Ask Tronald Dump, President of the USA, about a range of topics. Quotes courtesy of the tronalddump.io API.
 
 ## Examples
- - "What does tronald think about apologies?"
- - "What does tronald have to say about the economy?"
-
+* "What does Tronald Dump think about Obama?"
+* "What does Tronald Dump have to say about the economy?"
+* "Tronald what do you think about guns?"
+* "Tronald tell me about Hillary?"
 
 ## Credits
-Kris Gesling
+Kris Gesling (@krisgesling)
+and thanks to [Mathias Schilling](https://twitter.com/MatChilling) for the tronalddump.io API.
 
+## Category
+**Entertainment**
 
+## Tags
+#trump
+#tronald
+#tronalddump
--- __init__.py
+++ __init__.py
@@ -1,5 +1,11 @@
+from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.util.log import LOG
+import urllib, urllib.request, json, random, re
+from urllib.request import Request
 
+__author__ = 'krisgesling'
+LOGGER = LOG(__name__)
 
 class TronaldDump(MycroftSkill):
     def __init__(self):
@@ -7,9 +13,26 @@ class TronaldDump(MycroftSkill):
 
     @intent_file_handler('dump.tronald.intent')
     def handle_dump_tronald(self, message):
-        self.speak_dialog('dump.tronald')
+        topic = message.data['topic']
+        urlParams = { 'query': topic }
+        apiUrl = "https://api.tronalddump.io/search/quote?%s" % urllib.parse.urlencode(urlParams)
+        try:
+            with urllib.request.urlopen(Request(apiUrl, headers={'User-Agent': 'Mozilla/5.0'})) as url:
+                result = json.loads(url.read().decode())
+                if result['count'] == 0:
+                    self.speak_dialog('no.result', {'topic': topic})
+                else:
+                    i = random.randint(0, result['count'] - 1)
+                    rawQuote = result['_embedded']['quotes'][i]['value']
+                    speakingQuote = re.sub(r'https?:\/\/.*\s?', '', rawQuote)
+                    self.speak_dialog('dump.tronald', {'quote': speakingQuote})
+        except urllib.error.HTTPError as e:
+            self.speak_dialog('http.error')
+        except urllib.error.URLError as e:
+            self.speak_dialog('url.error')
 
+    def stop(self):
+        pass
 
 def create_skill():
     return TronaldDump()
-
