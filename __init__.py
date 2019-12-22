3cb953f8d0217a72ecd597255d3d4936b371b6ba
Kris Gesling <krisgesling@gmail.com>
2019-12-22 14:04:01 +0930

    cleanup old code

--- __init__.py
+++ __init__.py
@@ -1,23 +1,17 @@
 from adapt.intent import IntentBuilder
-from mycroft import MycroftSkill, intent_file_handler
-from mycroft.util.log import LOG
-import urllib, urllib.request, json, random, re
-from urllib.request import Request
-
-__author__ = 'krisgesling'
-LOGGER = LOG(__name__)
+from mycroft import MycroftSkill, intent_handler
+import urllib, json, random, re
+from urllib.request import Request, urlopen
 
 class TronaldDump(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
 
-    @intent_file_handler('dump.tronald.intent')
+    @intent_handler('dump.tronald.intent')
     def handle_dump_tronald(self, message):
         topic = message.data.get('topic', '')
         urlParams = { 'query': topic }
         apiUrl = "https://api.tronalddump.io/search/quote?%s" % urllib.parse.urlencode(urlParams)
         try:
-            with urllib.request.urlopen(Request(apiUrl, headers={'User-Agent': 'Mozilla/5.0'})) as url:
+            with urlopen(Request(apiUrl, headers={'User-Agent': 'Mozilla/5.0'})) as url:
                 result = json.loads(url.read().decode())
                 if result['count'] == 0:
                     self.speak_dialog('no.result', {'topic': topic})
@@ -30,9 +24,6 @@ class TronaldDump(MycroftSkill):
             self.speak_dialog('http.error')
         except urllib.error.URLError as e:
             self.speak_dialog('url.error')
-
-    def stop(self):
-        pass
 
 def create_skill():
     return TronaldDump()
