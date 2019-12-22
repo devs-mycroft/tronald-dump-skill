be229f37f37a36356af033c7ed87a58b07bf1801
Kris Gesling <krisgesling@gmail.com>
2019-12-22 14:13:32 +0930

    refactor - pythonify

--- __init__.py
+++ __init__.py
@@ -8,22 +8,26 @@ class TronaldDump(MycroftSkill):
     @intent_handler('dump.tronald.intent')
     def handle_dump_tronald(self, message):
         topic = message.data.get('topic', '')
-        urlParams = { 'query': topic }
-        apiUrl = "https://api.tronalddump.io/search/quote?%s" % urllib.parse.urlencode(urlParams)
+        url_params = urllib.parse.urlencode({ 'query': topic })
+        api_url = 'https://api.tronalddump.io/search/quote?%s' % url_params
+        headers = {'User-Agent': 'Mozilla/5.0'}
         try:
-            with urlopen(Request(apiUrl, headers={'User-Agent': 'Mozilla/5.0'})) as url:
+            with urlopen(Request(api_url, headers)) as url:
                 result = json.loads(url.read().decode())
                 if result['count'] == 0:
                     self.speak_dialog('no.result', {'topic': topic})
                 else:
                     i = random.randint(0, result['count'] - 1)
-                    rawQuote = result['_embedded']['quotes'][i]['value']
-                    speakingQuote = re.sub(r'https?:\/\/.*\s?', '', rawQuote)
-                    self.speak(speakingQuote)
+                    raw_quote = result['_embedded']['quotes'][i]['value']
+                    speakable_quote = remove_http_links(raw_quote)
+                    self.speak(speakable_quote)
         except urllib.error.HTTPError as e:
             self.speak_dialog('http.error')
         except urllib.error.URLError as e:
             self.speak_dialog('url.error')
+
+def remove_http_links(raw_quote):
+    return re.sub(r'https?:\/\/.*\s?', '', raw_quote)
 
 def create_skill():
     return TronaldDump()
